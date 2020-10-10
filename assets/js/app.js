ff0f1a2431679c8d95b0337d6e708af5cd3b650d
Moisés Costa <moisesmengo@live.com>
2020-10-10 13:21:30 -0300

    finalizando modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,36 @@
 const vm = new Vue({
   el: "#app",
   data: {
-    produtos: []
+    produtos: [],
+    produto: false
+  },
+  filters: {
+    numeroPareco(valor) {
+      return valor.toLocaleString("pt-br", {
+        style: "currency", currency: "BRL"
+      })
+    }
   },
   methods: {
     fatchProdutos() {
       fetch("./api/produtos.json")
         .then(r => r.json())
         .then(r => this.produtos = r)
+    },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then(r => r.json())
+        .then(r => this.produto = r)
+    },
+    abrirModal(id) {
+      this.fetchProduto(id)
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false
     }
   },
   created() {
